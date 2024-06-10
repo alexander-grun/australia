9c9a621d4f96e42886a8e7a0857991ec5194b9ad
alex <vintego92@gmail.com>
2024-06-10 15:14:26 +0300

    1

--- table.py
+++ table.py
@@ -4,5 +4,5 @@ from streamlit_gsheets import GSheetsConnection
 
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-data = conn.read(spreadsheet=url, usecols=[0, 1, 2])
+data = conn.read(usecols=[0, 1, 2])
 st.dataframe(data)
